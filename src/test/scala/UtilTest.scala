3a2b899bb8a527068dcbfa9c6d5961cd2fc78e51
Diogo Barros <diogofpbarros@gmail.com>
2024-04-24 21:00:32 +0100

    implemented tests for Util.parseDollarPrice

--- src/test/scala/UtilTest.scala
+++ src/test/scala/UtilTest.scala
@@ -2,12 +2,11 @@
 import org.scalatest.funsuite.AnyFunSuite
 
 class UtilTest extends AnyFunSuite {
-    
-    class withAlmostEquals(d:Double) {
-        def ~=(y: Double)(implicit precision: Double) = (d - y).abs <= precision
+
+    val DELTA = 0.0001
+    def compareDoubles(x: Double, y: Double): Boolean = {
+        (x - y).abs < DELTA
     }
-    implicit val precision = 0.00001
-    implicit def add_~=(d:Double) = new withAlmostEquals(d)
 
     test("Util.parseSizeInMB.variesWithDevice") {
         assert(Util.parseSizeInMB("Varies with device") == None)
@@ -16,18 +15,34 @@ class UtilTest extends AnyFunSuite {
     test("Util.parseSizeInMB.gb") {
         val res = Util.parseSizeInMB("1.2G")
         assert(res.isDefined)
-        assert(res.get ~= 1200.0)
+        assert(compareDoubles(res.get, 1200.0))
     }
 
     test("Util.parseSizeInMB.mb") {
         val res = Util.parseSizeInMB("1.2M")
         assert(res.isDefined)
-        assert(res.get ~= 1.2)
+        assert(compareDoubles(res.get, 1.2))
     }
 
     test("Util.parseSizeInMB.kb") {
         val res = Util.parseSizeInMB("1.2k")
         assert(res.isDefined)
-        assert(res.get ~= 0.0012)
+        assert(compareDoubles(res.get, 0.0012))
+    }
+
+    test("Util.parseDollarPrice.zero") {
+        val res = Util.parseDollarPrice("0")
+        assert(res.isDefined)
+        assert(compareDoubles(res.get, 0))
+    }
+
+    test("Util.parseDollarPrice.valid") {
+        val res = Util.parseDollarPrice("$4.99")
+        assert(res.isDefined)
+        assert(compareDoubles(res.get, 4.99))
+    }
+
+    test("Util.parseDollarPrice.invalid") {
+        assert(Util.parseDollarPrice("") == None)
     }
 }
