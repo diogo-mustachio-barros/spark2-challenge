575d2ae6c169ff06cb492576c76882471d3601bc
Diogo Barros <diogofpbarros@gmail.com>
2024-04-25 12:47:41 +0100

    cleaning code

--- src/test/scala/SparkTest.scala
+++ src/test/scala/SparkTest.scala
@@ -9,58 +9,6 @@ import org.apache.spark.sql.types._
 
 class SparkTest extends AnyFunSuite with DataFrameSuiteBase {
 
-    // var spark: SparkSession = null
-    // var appsDF: DataFrame = null
-    // var userReviewsDF: DataFrame = null
-    
-    // override def beforeAll(): Unit = {
-    //     spark = SparkSession.builder()
-    //         .appName("DataFrameInitialization")
-    //         .getOrCreate()
-
-    //     val data = List(
-    //         Row("1", "John", "30"),
-    //         Row("2", "Alice", "25"),
-    //         Row("3", "Bob", "35")
-    //     )
-
-    //     val schema = StructType(
-    //         List(
-    //             StructField("id", StringType, nullable = true),
-    //             StructField("name", StringType, nullable = true),
-    //             StructField("age", StringType, nullable = true)
-    //         )
-    //     )
-
-    //     appsDF = spark.createDataFrame(data.asJava, schema)
-    // }
-
-    // test ("DFs equal") {
-    //     val expected=sc.parallelize(List(
-    //       Row("Wodehouse","Sales",250,1),
-    //       Row("Steinbeck","Sales",100,2),
-    //       Row("Hemingway","IT",349,1),
-    //       Row("Woolf","IT",99,2)
-    //     ))
-
-    //     val schema=StructType(
-    //       List(
-    //       StructField("emp",StringType,true),
-    //       StructField("dept",StringType,true),
-    //       StructField("sal",IntegerType,false),
-    //       StructField("rank",IntegerType,true)
-    //       )
-    //     )
-
-    //     val e2=sqlContext.createDataFrame(expected,schema)
-    //     val actual=ScratchPad.get_data_frame(sqlContext.sparkSession)
-    //     assertDataFrameEquals(e2,actual)
-    //   }
-
-    // override def afterAll(): Unit = {
-    //     spark.stop()
-    // }
-
     val DELTA: Double = 0.0001
 
     val USER_REVIEWS_SCHEMA = StructType(List(
@@ -70,19 +18,6 @@ class SparkTest extends AnyFunSuite with DataFrameSuiteBase {
         StructField(Spark.SENTIMENT_POLARITY_HEADER, StringType, true),
         StructField(Spark.SENTIMENT_SUBJECTIVITY_HEADER, StringType, true)
     ))
-
-    test("simple test") {
-        val sqlCtx = sqlContext
-        import sqlCtx.implicits._
-
-        val input1 = sc.parallelize(List[(Int, Double)]((1, 1.1), (2, 2.2), (3, 3.3))).toDF
-        val input2 = sc.parallelize(List[(Int, Double)]((1, 1.2), (2, 2.3), (3, 3.4))).toDF
-        assertDataFrameApproximateEquals(input1, input2, 0.11) // equal
-
-        // intercept[org.scalatest.exceptions.TestFailedException] {
-        //     assertDataFrameApproximateEquals(input1, input2, 0.05) // not equal
-        // }
-    }
 
     test("Spark.getAverageSentimentPolarityByApp.validNeutral") {
         // Input data for the DataFrame
